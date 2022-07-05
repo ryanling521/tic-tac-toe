1fc9fab9051f3e01d50cfa918f41aa05c1de4256
Ryan <ryanling521@g.ucla.edu>
2022-07-04 18:42:06 -0700

    alternating X/O, need to add winning logic

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-// gameboard module
+const cells = document.querySelectorAll('.cell');
+const container = document.querySelector(".container");
+const button = document.querySelector('#btn');
+const cell1 = document.querySelector('#cell-1');
+const cell2 = document.querySelector('#cell-2');
+const cell3 = document.querySelector('#cell-3');
+const cell4 = document.querySelector('#cell-4');
+const cell5 = document.querySelector('#cell-5');
+const cell6 = document.querySelector('#cell-6');
+const cell7 = document.querySelector('#cell-7');
+const cell8 = document.querySelector('#cell-8');
+const cell9 = document.querySelector('#cell-9');
+
+// X/O array
 const gameBoardModule = (() => {
-    let gameBoard = ['X','O'];
+    let gameBoard = [];
+
     return {gameBoard};
 })();
+
+// displays X/O
+const displayController = (() => {
+    index = 0;
+    cells.forEach(cell => {
+        cell.addEventListener('click', displayArray);
+
+        function displayArray() {
+            if (index % 2 == 0) {
+                cell.textContent = gameBoardModule.gameBoard[0];
+            } else {
+                cell.textContent = gameBoardModule.gameBoard[1];
+            }
+            index++;            
+        }
+    })
+})();
+
+// check winner
+const checkWinner = (() =>  {
+    if ( (cell1.value == cell2.value) && (cell2.value == cell3.value) && (cell1.value != null)) {
+        console.log('winner!');
+    }
+})();
+
 
 // players factory function
 function CreatePlayer(name, assignedXO) {
@@ -14,30 +53,50 @@ function CreatePlayer(name, assignedXO) {
     };
 }
 
-const ryan = CreatePlayer('ryan', 'X');
-console.log(ryan.name);
-console.log(ryan.assignedXO);
+const displayPlayer = (() => {
+    const name = document.querySelector('#name');
+    const xButton = document.querySelector('#X');
+    const oButton = document.querySelector('#O');
 
-// display module
-const displayArray = (() => {
-    const cells = document.querySelectorAll('.cell');
-    cells[0].textContent = gameBoardModule.gameBoard[0];
-    cells[1].textContent = gameBoardModule.gameBoard[1];
-    return {};
+    const removeAllChildNodes = (parent) => {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    };
+
+    button.addEventListener('click', displayPlayer);
+
+    function displayPlayer() {
+        let newDiv = document.createElement('div');
+        removeAllChildNodes(container);
+
+        if (xButton.checked) {
+            const buttonValue = 'X';
+            newDiv.textContent =`Player one: ${name.value} ${buttonValue}`;
+            let player1 = CreatePlayer(name.value, buttonValue);
+            let player2 = CreatePlayer(name.value, 'O');
+            gameBoardModule.gameBoard.push(buttonValue);
+            gameBoardModule.gameBoard.push('O');
+        } else if (oButton.checked) {
+            const buttonValue = 'O';
+            newDiv.textContent =`Player one: ${name.value} ${buttonValue}`;
+            let player1 = CreatePlayer(name.value, buttonValue);
+            let player2 = CreatePlayer(name.value, 'X');
+            gameBoardModule.gameBoard.push(buttonValue);
+            gameBoardModule.gameBoard.push('X');
+        } else {
+            newDiv.textContent =`Please select your symbol!`;
+        }
+
+        container.append(newDiv); 
+        form.reset();   
+        
+        return {
+            displayPlayer,
+        }
+    }
 })();
 
 
-// module pattern
-const object = (() => {
-    const add = (a, b) => a + b;
-    const sub = (a, b) => a - b;
-    const mul = (a, b) => a * b;
-    const div = (a, b) => a / b;
-    return {
-      add,
-      sub,
-      mul,
-      div,
-    };
-  })();
+
 
